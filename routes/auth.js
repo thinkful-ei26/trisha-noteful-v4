3e2fc1c280a67caa8c610b711ca06b988b5e09e2
kronicle114 <trisha.aguinaldo@gmail.com>
2018-12-04 15:02:38 -0500

    authToken working on POSTMAN

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,13 @@
 
 const express = require('express');
 const passport = require('passport');
-const {  jwt, JWT_SECRET, JWT_EXPIRY } = require('jsonwebtoken');
+const jwt= require('jsonwebtoken');
+
+const { JWT_SECRET, JWT_EXPIRY } = require('../config');
 
 const router = express.Router();
 
+//jwt.sign() invokes the User schema toJSON() which removes the password and __v from payload
 const createAuthToken = (user) => {
   return jwt.sign( { user }, JWT_SECRET, {
     subject: user.username,
@@ -24,9 +27,15 @@ const options = {
 
 const localAuth = passport.authenticate('local', options);
 
-/* POST on /api/login */
+// /* POST on /api/login */
+// router.post('/', localAuth, (req, res) => {
+//   return res.json(req.user);
+// });
+
 router.post('/', localAuth, (req, res) => {
-  return res.json(req.user);
+  const authToken = createAuthToken(req.user);
+  return res.json({ authToken });
 });
+
 
 module.exports = router;
