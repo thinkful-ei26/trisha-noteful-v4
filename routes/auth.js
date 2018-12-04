84d59cbfb9f5a62fe696d2efbdb84a91ba222324
kronicle114 <trisha.aguinaldo@gmail.com>
2018-12-04 14:49:03 -0500

    changed from toObjet to toJSON

--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,16 @@
 
 const express = require('express');
 const passport = require('passport');
+const {  jwt, JWT_SECRET, JWT_EXPIRY } = require('jsonwebtoken');
 
 const router = express.Router();
+
+const createAuthToken = (user) => {
+  return jwt.sign( { user }, JWT_SECRET, {
+    subject: user.username,
+    expiresIn: JWT_EXPIRY
+  });
+};
 
 /* 
   failWithError option configures the middleware to throw an error instead of automatically returning a 401 response. The error is then caught by the error handling middleware on server.js and returned as JSON.
